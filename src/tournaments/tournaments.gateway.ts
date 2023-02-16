321b39d3ca886aa978f822b0d22255e7bf0294c0
YogeshKumarBeniwal <46630702+YogeshKumarBeniwal@users.noreply.github.com>
2023-02-16 13:48:47 +0530

    Remove user from socket room on disconnect.

--- src/tournaments/tournaments.gateway.ts
+++ src/tournaments/tournaments.gateway.ts
@@ -5,6 +5,7 @@ import { AddScoreDto } from './dto/add-score.dto';
 import { SocketWithAuth } from './dto/join-tournament.dto';
 import { SocketConnectionType } from './socket.room.connection.enum';
 import { TournamentStatus } from './tournament-status.enum';
+import { Tournament } from './tournament.entity';
 import { TournamentsService } from './tournaments.service';
 
 @WebSocketGateway({
@@ -25,90 +26,111 @@ export class TournamentsGateway implements OnGatewayConnection, OnGatewayDisconn
       `Socket connected with userID: ${username}, tournamentID: ${tournamentId}`
     );
 
-    const updatedTournament = await this.tournamentsService.addParticipant({
-      username,
-      tournamentId,
-      userId
-    });
-    
+    let updatedTournament: Tournament;
+
+    try {
+      updatedTournament = await this.tournamentsService.addParticipant({
+        username,
+        tournamentId,
+        userId
+      });
+    }
+    catch (error) {
+      this.logger.debug(`[addScore] ${error}`);
+      client._error({ code: 4001, message: error.message });
+      return;
+    }
+
     const roomName = tournamentId;
     await client.join(roomName);
 
     const clientCount = this.io.adapter.rooms?.get(roomName)?.size ?? 0;
 
     this.logger.debug(
-      `userID: ${username} joined room with name: ${roomName}`,
+      `user: ${username} joined room with name: ${roomName}`,
     );
     this.logger.debug(
       `Total clients connected to room '${roomName}': ${clientCount}`,
     );
-    
-    this.io.to(roomName).emit('tournament_joined', {userCount: clientCount});
 
-    if(updatedTournament.status == TournamentStatus.IN_PROGRESS){
-      this.io.to(roomName).emit('tournament_start', {
-        username, 
-        userCount: clientCount,
-        type: SocketConnectionType.JOIN
-      });
+    this.io.to(roomName).emit('tournament_joined', updatedTournament);
+
+    if (updatedTournament.status == TournamentStatus.IN_PROGRESS) {
+      this.logger.debug(
+        `Tournament started id: ${updatedTournament.id}`
+      );
+      this.io.to(roomName).emit('tournament_start', updatedTournament);
     }
   }
 
   async handleDisconnect(client: SocketWithAuth): Promise<void> {
-    const sockets = this.io.sockets;
-
     const { tournamentId, username, userId } = client;
-    await this.tournamentsService.removeParticipant({
-      username,
-      userId,
-      tournamentId
-    });
-
+    const sockets = this.io.sockets;
     const roomName = tournamentId;
+
+    client.leave(roomName);
+
     const clientCount = this.io.adapter.rooms?.get(roomName)?.size ?? 0;
 
-    this.logger.log(`Disconnected socket id: ${client.id}`);
+    this.logger.debug(`Disconnected socket username: ${username}`);
     this.logger.debug(`Number of connected sockets: ${sockets.size}`);
     this.logger.debug(
       `Total clients connected to room '${roomName}': ${clientCount}`,
     );
 
-    this.io.to(roomName).emit('tournament_joined', 
-      {
-        username, 
-        userCount: clientCount,
-        type: SocketConnectionType.LEAVE
-      }
-    );
+    let updatedTournament: Tournament;
+    try {
+      updatedTournament = await this.tournamentsService.removeParticipant({
+        username,
+        userId,
+        tournamentId
+      });
+    }
+    catch (error) {
+      this.logger.debug(`[addScore] ${error}`);
+      client._error({ code: 4001, message: error.message });
+      return;
+    }
+
+    this.io.to(roomName).emit('tournament_joined', updatedTournament);
   }
 
   @SubscribeMessage('add_score')
-  async nominate(
+  async addScore(
     @MessageBody() addScoreDto: AddScoreDto,
     @ConnectedSocket() client: SocketWithAuth,
   ): Promise<void> {
 
-    const {tournamentId, username, userId} = client;
+    const { tournamentId, username, userId } = client;
 
     this.logger.debug(
-      `Attempting to add score for user ${username} to tournament ${tournamentId}`,
+      `Attempting to add score for user ${username} to tournament ${tournamentId}`
     );
 
-    const updatedTournament = await this.tournamentsService.addParticipantScore({
-      score: addScoreDto.score,
-      tournamentId,
-      userId
-    });
+    let updatedTournament: Tournament;
 
-    this.io.to(tournamentId).emit('score_updated', {username, score: addScoreDto.score});
-    
-    if(updatedTournament.status == TournamentStatus.DONE){
+    try {
+      updatedTournament = await this.tournamentsService.addParticipantScore({
+        score: addScoreDto.score,
+        tournamentId,
+        userId
+      });
+    }
+    catch (error) {
+      this.logger.debug(`[addScore] ${error}`);
+      client._error({ code: 4001, message: error.message });
+      return;
+    }
+
+    this.io.to(tournamentId).emit('score_updated', { username, score: addScoreDto.score });
+
+    if (updatedTournament.status == TournamentStatus.DONE) {
       this.logger.debug(
         `Tournament ended and the winner is ${username}`,
       );
 
       this.io.to(tournamentId).emit('tournament_end', {
-        tournamentId, 
+        tournamentId,
         username
       });
     }
