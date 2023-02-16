26181ebd36495ee41f4834e85fcc59240a01c9e0
YogeshKumarBeniwal <46630702+YogeshKumarBeniwal@users.noreply.github.com>
2023-02-16 13:45:39 +0530

    Increased expiry time out for JWT.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -19,7 +19,7 @@ import { User } from './user.entity';
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get('JWT_SECRET'),
         signOptions: {
-          expiresIn: 3600,
+          expiresIn: 43200,
         },
       }),
     }),
